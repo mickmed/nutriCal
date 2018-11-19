6845008583d54dfd620d8973d76d9d9159843a47
student_113 <student_113@PSXMA113s-Air.westell.com>
2018-11-19 15:54:44 -0500

    updates

--- js/nutrical.js
+++ js/nutrical.js
@@ -5,18 +5,10 @@ class Ingredient {
         this.servings_container = servings_container;
         this.calories = calories;
     }
-    speak() {
-        console.log(
-            `My name is $(this.fname),
-       I am ${this.humanYears()},
-       and I am a ${this.breed}`
-        );
-    }
     humanYears() {
         return this.age * 7;
     }
 }
-
 
 let cheese = new Ingredient('cheese', '3/4  cup', 8, 12);
 let cashews = new Ingredient('cashews', '.5 cup', 8, 14);
@@ -24,18 +16,14 @@ let whole_wheat_flour = new Ingredient('whole_wheat_flour', '.5 cup', 8, 14);
 let lettuce_romain = new Ingredient('lettuce, romaine', '.5 cup', 8, 14);
 let tempeh = new Ingredient('tempeh', '.5 cup', 8, 14);
 
-
-
 console.log(cheese.serving_size);
 
 var ingredients = [];
 ingredients.push(cheese, cashews, whole_wheat_flour, lettuce_romain, tempeh);
-console.log(ingredients);
 
-
-
+//MAKE DROPDOWN OF INGREDIENTS
 for (ing in ingredients) {
-    console.log(ingredients[ing].ing_name);
+    // console.log(ingredients[ing].ing_name);
     var option = document.createElement("option");
 
     option.value = ingredients[ing].ing_name;
@@ -44,49 +32,37 @@ for (ing in ingredients) {
     document.getElementById("ings").appendChild(option);
 }
 
+//JQuery USED TO ADD AND REMOVE DYNAMIC INGREDIENTS LIST
+$(document).ready(function() {
+    $("#ings").change(function() {
+    	var x = $('#ings option:selected').val();
+        $("#ings-list").append("<li>" + x + "<a href='javascript:void(0);' class='remove'>&times;</a></li>");
+    });
+    //need on() here to delete dynamically created <li>
+    $(document).on("click", "a.remove", function() {
+        $(this).parent().remove();
+    });
+});
 
+// TRIED TO ADD INGREDIENTS TO UL WITH VANILLA JS BUT COULD NOT REMOVE DYNAMICALLY CREATED <li>'s
 
-// ADD INGREDIENTS TO UL ON APP PAGE
-var add_ing_btn = document.getElementById('add-ing-btn');
-console.log(add_ing_btn);
-var addIngredient = function() {
-    var ings_value = document.getElementById("ings").value;
-    var create_li_ing = document.createElement('li');
-    create_li_ing.id = ings_value;
-    create_li_ing.innerHTML = ings_value;
-    document.getElementById('todo-list').appendChild(create_li_ing);
-   
-    // var create_li_X = document.createElement('li');
-    // create_li_X.innerHTML = " X";
-    // var i = document.getElementById('x-out').appendChild(create_li_X);
-    // console.log(i);
-    console.log(document.getElementsByTagName('ings_'));
-    var add_x = document.getElementById('x-out');
-	add_x.style.display="block";
-}
+// get add item button
+// var add_ing_btn = document.getElementById('add-ing-btn');
 
-add_ing_btn.addEventListener('click', addIngredient);
+// var addIngredient = function() {
+//     var ings_value = document.getElementById("ings").value;
+//     var create_li_ing = document.createElement('li');
+//     create_li_ing.id = ings_value;
+//     create_li_ing.innerHTML = ings_value;
+//     document.getElementById('todo-list').appendChild(create_li_ing);
 
-
-//REMOVE INGREDIENT FROM UL
-// var remIngredient = function(){
-// 	// var Xout = document.getElementById('x-out');
-// 	// Xout.parentNode.removeChild(Xout);
-
-
+//     var create_li_X = document.createElement('li');
+//     create_li_X.innerHTML = " X";
+//     var i = document.getElementById('x-out').appendChild(create_li_X);
 // }
 
-// add_ing_btn.addEventListener('click', remIngredient);
+// add_ing_btn.addEventListener('click', addIngredient);
 
-var add_y = document.getElementById('x-out');
-add_y.style.display="none";
-
-// 
-
-
-
-
-// 
 
 
 
