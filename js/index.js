7eabf60ece639b8955329d175e9df4c8121ad90b
student_113 <student_113@PSXMA113s-Air.westell.com>
2018-11-24 19:27:11 -0500

    stuff

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-
 // GET PAGE URL END
 let urlSnipper = (path) => {
     page = path.split("/").pop();
@@ -7,70 +6,100 @@ let urlSnipper = (path) => {
 let pathName = window.location.pathname;
 let sitePathPageName = urlSnipper(pathName);
 
-
-if(sitePathPageName === 'index.html'){
+//////INDEX PAGE/////////////
+//cycle words in banner
+if (sitePathPageName === 'index.html') {
     console.log('here');
-let bannerTargeting = () => {
-    
-    var banner = document.getElementById("banner-home");
-    // var pos = 0;
-    var targeting = setInterval(bannerChange, 5000);
-    function bannerChange() {
-        
-        if (banner.innerHTML.indexOf("planning") !== -1) {
-            console.log('planning');
-            banner.innerHTML = "<h1>targeting nutrients?</h1>";
-            clearInterval(targeting);
-            bannerCounting();
-        }
-    }
-}
-// console.log('heres');
+    let bannerTargeting = () => {
 
-let bannerCounting = () => {
-    // console.log('here');
-    var banner = document.getElementById("banner-home");
-    // var pos = 0;
-    var counting = setInterval(bannerChange, 5000);
-    function bannerChange() {
-        // banner.innerHTML="ha"; 
-        if (banner.innerHTML.indexOf("targeting") !== -1) {
-            console.log('targeting');
-            banner.innerHTML = "<h1>counting calories?</h1>";
-            clearInterval(counting);
-            bannerSmallBus();
+        var banner = document.getElementById("banner-home");
+        // var pos = 0;
+        var targeting = setInterval(bannerChange, 5000);
+
+        function bannerChange() {
+
+            if (banner.innerHTML.indexOf("planning") !== -1) {
+                console.log('planning');
+                banner.innerHTML = "<h1>targeting nutrients?</h1>";
+                clearInterval(targeting);
+                bannerCounting();
+            }
         }
     }
+    // console.log('heres');
+
+    let bannerCounting = () => {
+        // console.log('here');
+        var banner = document.getElementById("banner-home");
+        // var pos = 0;
+        var counting = setInterval(bannerChange, 5000);
+
+        function bannerChange() {
+            // banner.innerHTML="ha"; 
+            if (banner.innerHTML.indexOf("targeting") !== -1) {
+                console.log('targeting');
+                banner.innerHTML = "<h1>counting calories?</h1>";
+                clearInterval(counting);
+                bannerSmallBus();
+            }
+        }
+    }
+
+    let bannerSmallBus = () => {
+        var banner = document.getElementById("banner-home");
+        // var pos = 0;
+        var smallBus = setInterval(bannerChange, 5000);
+
+        function bannerChange() {
+            // banner.innerHTML="ha"; 
+            if (banner.innerHTML.indexOf("counting") !== -1) {
+                console.log('targeting');
+                banner.innerHTML = "<h1>small business?</h1>";
+                clearInterval(smallBus);
+                bannerPlanning();
+            }
+        }
+    }
+    let bannerPlanning = () => {
+        var banner = document.getElementById("banner-home");
+        // var pos = 0;
+        var planning = setInterval(bannerChange, 5000);
+
+        function bannerChange() {
+            // banner.innerHTML="ha"; 
+            if (banner.innerHTML.indexOf("small") !== -1) {
+                console.log('targeting');
+                banner.innerHTML = "<h1>planning healthy meals?</h1>";
+                clearInterval(planning);
+                bannerTargeting();
+            }
+        }
+    }
+    bannerTargeting();
 }
 
-let bannerSmallBus = () => {
-    var banner = document.getElementById("banner-home");
-    // var pos = 0;
-    var smallBus = setInterval(bannerChange, 5000);
-    function bannerChange() {
-        // banner.innerHTML="ha"; 
-        if (banner.innerHTML.indexOf("counting") !== -1) {
-            console.log('targeting');
-            banner.innerHTML = "<h1>small business?</h1>";
-            clearInterval(smallBus);
-            bannerPlanning();
-        }
+/////////ABOUT PAGE////////////////////////////////
+//modal for legal terms
+if (sitePathPageName === 'about.html') {
+    var modal = document.getElementById('legal-modal');
+    var modalBtn = document.getElementById("terms");
+console.log(modalBtn);
+    var close = document.getElementById("close-modal");
+    modalBtn.onclick = () => {
+        modal.style.display = "block";
+        var wrapper = document.getElementsByClassName('main-about');
+        console.log(wrapper);
+        wrapper[0].style.display.backgroundColor = "red";
     }
-}
-let bannerPlanning = () => {
-    var banner = document.getElementById("banner-home");
-    // var pos = 0;
-    var  planning = setInterval(bannerChange, 5000);
-    function bannerChange() {
-        // banner.innerHTML="ha"; 
-        if (banner.innerHTML.indexOf("small") !== -1) {
-            console.log('targeting');
-            banner.innerHTML = "<h1>planning healthy meals?</h1>";
-            clearInterval(planning);
-            bannerTargeting();
-        }
+    close.onclick = () => {
+        modal.style.display = "none";
     }
-}
-bannerTargeting();
-}
+    // close modal from outside modal
+    window.onclick = (event) => {
+        console.log('window');
+        if (event.target == modal) {
+            modal.style.display = "none";
 
+        }
+    }
+}
