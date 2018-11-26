34acc8d71c1b2e1ff10f3208794470753a5ec9e9
student_113 <student_113@PSXMA113s-Air.westell.com>
2018-11-26 05:58:17 -0500

    almost there

--- js/app.js
+++ js/app.js
@@ -37,22 +37,25 @@ class Ingredient {
         this.totalFat = totalFat;
         this.totalCarb = totalCarb;
         this.protein = protein;
-
+        
     }
 }
 
 let cheese = new Ingredient('cheese', 'cheese', .125, 'cup', 8, 110, 9, 2, 7);
 let cashews = new Ingredient('cashews', 'cashews', .25, 'cup', 15, 180, 13, 9, 7);
 let wholeWheatPasta = new Ingredient('wholeWheatPasta', 'pasta - whole wheat', .75, 'cup', 8, 190, 1.5, 41, 7);
-let milkHalfHalf = new Ingredient('milkHalfHalf', 'milk - half and half', 1, 'tbsp', 32, 30, 1.5, 4, 0);
+let milkHalfHalf = new Ingredient('milkHalfHalf', 'milk - half and half', 1, 'tbsp', 32, 30, 1.5, 4, 8);
 let tempeh = new Ingredient('tempeh', 'tempeh', .5, 'cup', 2, 240, 11, 16, 20);
 let pastaSauce = new Ingredient('pastaSauce', 'pasta sauce', .5, 'cup', 6, 60, 2, 8, 2);
 let hummus = new Ingredient('hummus', 'hummus', 2, 'tbsp', 8, 70, 5, 4, 2);
 let wholeWheatWraps = new Ingredient('wholeWheatWraps', 'wraps - whole wheat', 50, 'grms', 6, 130, 3, 21, 6);
+let lettuce = new Ingredient('lettuce', 'lettuce', .25, 'cup', 4, 4.2, 0, .8, .4);
+let tomatoes = new Ingredient('tomatoes', 'tomatoes', .25, 'cup', 1, 25.3, .3, 5, 1.8);
+let walnuts = new Ingredient('walnuts', 'walnuts', 1, 'cup', 4, 765, 76.3, 16, 17.8);
 
 /////MAKE ARRAY OF INGREDIENTS FOR DROPDOWN
 let ingredients = [];
-ingredients.push(cheese, cashews, wholeWheatPasta, milkHalfHalf, tempeh, pastaSauce, hummus, wholeWheatWraps);
+ingredients.push(cheese, cashews, wholeWheatPasta, milkHalfHalf, tempeh, pastaSauce, hummus, wholeWheatWraps, lettuce, tomatoes, walnuts);
 
 //MAKE DROPDOWN OF 
 
@@ -142,34 +145,14 @@ let addIngObj = document.getElementById("ings").addEventListener("change", funct
         document.getElementById(objKeys[i]).innerHTML = obj[objKeys[i]];
     }
 
-    console.log(recipe.ingredients);
-
+    // console.log(recipe.ingredients);
+    
+    currentIngVals.ingId = id;
+    currentIngVals.ingAmnt = amnt;
+    currentIngVals.ingMeas = 'tsp';
+    // console.log(currentIngVals.storedObjs);
+    currentIngVals.stored();
 });
-
-
-//stored individual ing amounts after changes are made
-// var currentIngAmntsStored = {
-//     ingIds: [],
-//     ingIdsObj: {
-//     ingId:{ingAmnt: '', ingMeas: ''}
-
-//     },
-
-//     addIngredient: function(ingId, ingAmnt, ingMeas) {
-//         this.ingIdsObj.ingId.push({ingId:});
-
-//         this.ingIdsObj.ingId.ingAmnt = ingAmnt;
-//         this.ingIdsObj.ingId.Meas = ingMeas;
-//     },
-//     addVals: function(ingId, ingAmnt, ingMeas){
-//         this.ingIdsObj.ingId.ingAmnt = ingAmnt;
-//         this.ingIdsObj.ingId.Meas = Meas;
-//     }
-// };
-
-
-
-
 
 ///swaps out current values in label as changes are made to particlual item in list
 let currentIngVals = {
@@ -180,35 +163,41 @@ let currentIngVals = {
 
     stored: function() {
         //store object values for later
-        // console.log(currentIngAmntsStored.ingIds);
-        this.storedObjs.push(this.ingId, this.ingAmnt, this.ingMeas);
 
-
+        if (this.storedObjs.length !== 0) {
+            console.log(this.storedObjs.length);
+            for (var i = 0; i < this.storedObjs.length; i++) {
+                if (this.storedObjs[i].ingId === this.ingId) {
+                    console.log('yes match', i, this.ingId);
+                    // console.log(this.storedObjs[i].ingId);
+                    this.storedObjs[i].ingAmnt = this.ingAmnt;
+                    this.storedObjs[i].ingMeas = this.ingMeas;
+                    // console.log(this.storedObjs[i].ingAmnt);
+                    // console.log(i, 'yes');
+                    
+                    return true;
+                    // console.log(this.storedObjs[i].ingId.indexOf(this.ingId));
+                    // console.log(this.storedObjs);
+                    // console.log(this.storedObjs);
+                    // console.log(this.ingId);
+                    // console.log(this.storedObjs[i]);
+                } else {
+                    console.log(i, 'no');
+                    // console.log(this.ingId);
+                    // console.log(this.storedObjs[i]);
+                    // console.log(this.storedObjs[i].ingId);
+                    
+            }
+            // console.log('here');
+            
+            }
+            this.storedObjs.push({ ingId: this.ingId, ingAmnt: this.ingAmnt, ingMeas: this.ingMeas });
+        } else {
+          
+            this.storedObjs.push({ ingId: this.ingId, ingAmnt: this.ingAmnt, ingMeas: this.ingMeas });
+        }
         
 
-        // function userExists(username) {
-        //     return this.storedObjs.some(function(el) {
-        //         return el.username === username;
-        //     });
-        // }
-
-        // console.log(userExists(this.ingId)); // true
-
-        // currentIngAmntsStored.addIngredient(this.ingId, this.ingAmnt, this.ingMeas);
-        
-
-        console.log(this.storedObjs);
-        // if(currentIngAmntsStored.ingIds.includes(this.ingId)){
-        //     console.log('yes');
-        //     currentIngAmntsStored.addVals(this.ingId, this.ingAmnt, this.ingMeas);
-        // }
-        // else{
-        //     console.log('no');
-        //     console.log('here');
-        //     console.log(this.ingId);
-
-        // }
-        console.log(this.storedObjs.some(checkObjId));
 
     }
 };
@@ -220,18 +209,53 @@ let getFocus = (elem) => {
     let objId = eval(elem.dataset.id);
     let objKeys = Object.keys(eval(elem.dataset.id));
     let objVals = Object.values(eval(elem.dataset.id));
-    for (i = 0; i < objVals.length; i++) {
+    // console.log(objId);
+    // console.log(currentIngVals);
+    // console.log(elem.previousSibling.previousSibling.value);
+    let amnt = elem.previousSibling.previousSibling.value;
+    let meas = elem.previousSibling.value;
+    // calculate new values after input change
+    for (i = 0; i < objKeys.length; i++) {
+        // fill the label with new values
         document.getElementById(objKeys[i]).innerHTML = objVals[i];
     }
+
+  
+    // calculate new values after input change
+    for (i = 0; i < objVals.length; i++) {
+        if (typeof objVals[i] === 'number') {
+            //make orig obj amnts into single tsp amnts
+            let lowestCommonByTsp = (objVals[i] / objId.servAmnt);
+
+            //times them by the new amnt val
+            var newIngAmnt = lowestCommonByTsp * amnt;
+            // console.log(amnt);
+            //times them by the amnt meas
+            if (currentIngVals.ingMeas === 'tbs') {
+                newIngAmnt = newIngAmnt * 3;
+            }
+            if (currentIngVals.ingMeas === 'cup') {
+                newIngAmnt = newIngAmnt * 48;
+            }
+            newIngAmnt = parseFloat(newIngAmnt).toFixed(0);
+        } else {
+            var newIngAmnt = objVals[i];
+        }
+        // fill the label with new values
+        document.getElementById(objKeys[i]).innerHTML = newIngAmnt;
+
+    }
+  
+    
 
 }
 // when click on amnt to change
 let chngAmnt = (amnt) => {
-    
+
     let objId = eval(amnt.dataset.id);
     let objKeys = Object.keys(eval(amnt.dataset.id));
     let objVals = Object.values(eval(amnt.dataset.id));
-
+    // console.log(currentIngVals);
     // set object with original obj meas if empty
     if (currentIngVals.ingMeas === '') {
         currentIngVals.ingMeas = objId.servMeas; //set to original object val
@@ -240,6 +264,8 @@ let chngAmnt = (amnt) => {
     for (i = 0; i < objVals.length; i++) {
         if (typeof objVals[i] === 'number') {
             //make orig obj amnts into single tsp amnts
+            // console.log(objVals[i]);
+            // console.log(objId.servAmnt);
             let lowestCommonByTsp = (objVals[i] / objId.servAmnt);
             //times them by the new amnt val
             var newIngAmnt = lowestCommonByTsp * amnt.value;
@@ -256,19 +282,19 @@ let chngAmnt = (amnt) => {
         }
         // fill the label with new values
         document.getElementById(objKeys[i]).innerHTML = newIngAmnt;
-        // console.log(document.getElementById('servAmnt').innerHTML = amnt.value);
-        // console.log(amnt.value);
+
     }
     // set object with current value
     currentIngVals.ingId = objId.ingID;
     currentIngVals.ingAmnt = amnt.value;
     currentIngVals.stored();
+    // console.log(currentIngVals.storedObjs);
 
 
 }
 // when click on amnt to change
 let chngMeas = (meas) => {
-
+    console.log(eval(meas.dataset.id));
     let objId = eval(meas.dataset.id);
     let objKeys = Object.keys(eval(meas.dataset.id));
     let objVals = Object.values(eval(meas.dataset.id));
@@ -280,7 +306,7 @@ let chngMeas = (meas) => {
     // calculate new values after meas change
     for (i = 0; i < objVals.length; i++) {
         if (typeof objVals[i] === 'number') {
-            // console.log(objVals[i]);
+            console.log(objVals[i]);
             let lowestCommonByTsp = (objVals[i] / objId.servAmnt);
             // console.log(lowestCommonByTsp);
             var newIngAmnt = lowestCommonByTsp * currentIngVals.ingAmnt;
@@ -304,73 +330,66 @@ let chngMeas = (meas) => {
     }
     currentIngVals.ingName = objId;
     currentIngVals.ingMeas = meas.value;
-    console.log(currentIngVals.ingMeas);
+    currentIngVals.stored();
+    // console.log(currentIngVals.ingMeas);
 
-    console.log()
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function addRecipe(vals) {
-    let totalAmnts = document.getElementsByClassName('ings-list-amnt');
-    for (i = 0; i < totalAmnts.length; i++) {
-        recipeTotals.addTotals(totalAmnts[i], totalAmnts[i].nextSibling);
-        // console.log(totalAmnts);
-
-    }
-    console.log(recipeTotals.totals);
-
-    // console.log(recipeTotals.totals.reduce((a, b) => +a + +b.allAmnts, 0));
-
+    
 
 }
 
-let recipeTotals = {
-    totals: [],
-    addTotals: function(amnt, meas) {
-        console.log('data', amnt.dataset.id);
-        if (this.totals !== undefined || this.totals.length !== 0) {
 
-            console.log(this.totals);
-            for (tot in this.totals) {
-                console.log(this.totals[tot].dataID);
-                console.log
+let addRecipe = (vals) => {
+   var totalsArr = [];
+   var tot = [];
+    // console.log(currentIngVals.storedObjs);
+    for(stored in currentIngVals.storedObjs){
+        let currentId = currentIngVals.storedObjs[stored].ingId;
+        let currentAmnt = currentIngVals.storedObjs[stored].ingAmnt;
+        let currentMeas = currentIngVals.storedObjs[stored].ingMeas;
+        let objKeys =(Object.keys(eval(currentIngVals.storedObjs[stored].ingId)));
+        let objVals = (Object.values(eval(currentIngVals.storedObjs[stored].ingId)));
+        // console.log(objVals);
+        for (i = 0; i < objVals.length; i++) {
+            if (typeof objVals[i] === 'number') {
+               
+                // console.log(objVals[i]);
+                // console.log(typeof objVals[i]);
+                let lowestCommonByTsp = (objVals[i]/eval(currentId).servAmnt);
+                // console.log(eval(lowestCommonByTsp));
+                var newIngAmnt = lowestCommonByTsp * currentAmnt;
+                // console.log(newIngAmnt);
+     
+                if (currentMeas === 'tbs') {
+                    newIngAmnt = newIngAmnt * 3;
+
+                }
+                if (currentMeas === 'cup') {
+                    newIngAmnt = newIngAmnt * 48;
+
+                }
+                newIngAmnt = parseFloat(newIngAmnt).toFixed(0);
+
+                console.log(objKeys[i], newIngAmnt);
+                totalsArr.push({[objKeys[i]]: newIngAmnt});
+
+                tot[(objKeys[i])] += Number(newIngAmnt);
+                // console.log(typeof Number(objKeys[i]), typeof parseInt(newIngAmnt));
+            // } else {
+            //     var newIngAmnt = objVals[i];
+            // }
             }
         }
-        let amntsObj = { dataID: amnt.dataset.id, allAmnts: amnt.value, allMeas: meas.value };
-        this.totals.push(amntsObj);
-    },
-    getMeasFromSibling: function() {
-        // document.ge
-    }
+    }console.log(tot);
+
+    // console.log(recipeTotals.totals.reduce((a, b) => +a + +b.allAmnts, 0));
 }
+
+
+
+
+
+
+
 
 
 
@@ -385,208 +404,3 @@ let recipe = {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let currentObj = (currentIng) => {
-//     let obj = eval(currentIng);
-//     // console.log(obj);
-//     if (obj.servMeas === 'cup') {
-//         obj.servMeas = 'tsp';
-//         obj.servAmnt *= 48;
-//     }
-//     if (obj.servMeas === 'tbsp') {
-//         obj.servMeas = 'tsp';
-//         obj.servAmnt *= 3;
-//     }
-//     let amnt = obj.servAmnt;
-//     let meas = makeMeas();
-
-
-//     //JQuery USED TO ADD AND REMOVE DYNAMIC INGREDIENTS LIST
-//     let ingSelectedText = $("#ings option:selected").text();
-//     $("#ingredients-list").append(
-//         "<li><input type='text' class='ings-list-amnt' data-type='ing-amnt' onclick='chngAmnt(this)'value =" + amnt + "></select><select class='ings-list-meas'>" + meas + "</select><span class='ings-list-ing'>" + ingSelectedText + "</span><a href='javascript:void(0);' class='remove'>&nbsp&times;</a></li>");
-
-//     //need to use on() here to delete dynamically created <li>
-//     $(document).on("click", "a.remove", function() {
-//         $(this).parent().remove();
-//     });
-
-//     //loop through object keys and place values in Nutrition Facts Label
-
-//     let objKeys = Object.keys(eval(ingSelectedVal));
-//     for (i = 0; i < objKeys.length; i++) {
-//         // console.log(objKeys[i]);
-//         document.getElementById(objKeys[i]).innerHTML = obj[objKeys[i]];
-//     }
-
-// }
-
-
-
-
-// MAKE RECIPE - PUT INGREDIENTS INTO ARRAY
-
-
-
-//CYCLE THROUGH INGS AND ADD PROPERTIES
-$('#addPizza').on('click', function() {
-
-
-    let ings = [];
-
-
-    let sumCals = 0;
-    let sumTotFats = 0;
-    let sumTotCarbs = 0;
-    let sumTotProtein = 0;
-
-    for (ing in recipe.ingredients) {
-
-        ings.push(recipe.ingredients[ing].name);
-        var objKeys = Object.keys(eval(recipe.ingredients[ing].name));
-        var value = eval(recipe.ingredients[ing].name)[objKeys[i]];
-        // for (i = 0; i < objKeys.length; i++) {
-        //     var key = objKeys[i];
-        //     console.log(key);
-        //     console.log(eval(recipe.ingredients[ing].name)[objKeys[i]]);
-        //     classVals.push({
-        //         [key]: eval(recipe.ingredients[ing].name)[objKeys[i]] });
-        // }
-        console.log(typeof eval(recipe.ingredients[ing].name));
-        var cals = Number(eval(recipe.ingredients[ing].name).calories);
-        var totFat = Number(eval(recipe.ingredients[ing].name).totalFat);
-        var totCarb = Number(eval(recipe.ingredients[ing].name).totalCarb);
-        var totProtein = Number(eval(recipe.ingredients[ing].name).protein);
-
-
-
-        sumCals += cals;
-        sumTotFats += totFat;
-        sumTotCarbs += totCarb;
-        sumTotProtein += totProtein;
-    }
-    console.log(eval(recipe.ingredients[ing].name).calories);
-    // console.log(classVals);
-
-
-    // // let what = classVals.reduce((a, b) => +a + +b.calories, 0);
-    // // console.log(what);
-
-    // function shoonga(classVals) {
-    //     var total = 0;
-    //     for (var i = 0; i < classVals.length; i++) {
-    //         total = total + classVals[i].calories;
-    //     }
-    //     return total;
-    // }
-
-    // console.log(shoonga(classVals));
-
-});
-
-
-
-
-
-
-//         var localPizza = {
-//             toppings: pizza.toppings,
-//         }
-//         order.addPizza(localPizza);
-//     });
-
-
-//     var order = {
-//         pizzas: [],
-
-//         addPizza: function(pizza) {
-//             this.pizzas.push(pizza);
-//             this.updateOrder();
-//         },
-//         updateOrder: function() {
-//             //Let's build this together :)
-//             $('#order').html(''); //reset it first
-
-//             //create an output variable that we can add things to
-//             var output = '<li class="list-group-item list-group-item-success">Your Order</li>';
-//             for (index in this.pizzas) {
-//                 var localPizza = this.pizzas[index];
-//                 var pizzaNumber = parseInt(index) + 1;
-//                 var info = "Pizza #" + pizzaNumber + " has these toppings: ";
-//                 for (toppingIndex in localPizza.toppings) {
-//                     var localTopping = localPizza.toppings[toppingIndex];
-//                     info += localTopping.name + " | ";
-//                 }
-//                 output += '<li class="list-group-item">' + info + '</li>'
-
-//             }
-//             $('#order').html(output); //update the DOM with the new output
-
-//             //Reset global pizza
-//             pizza.toppings = [];
-//             // updateDomPizza(pizza);
-//         }
-//     }
-
-
-
-//     // function updateDomPizza(pizza)
-//     //     {
-//     //         // console.log('here');
-//     //         $('#pizza').html(''); //reset it first
-
-//     //         //create an output variable that we can add things to
-//     //         var output = 'Your Pizza';
-//     //          console.log('pizza', typeof pizza.toppings);
-//     //         for(index in pizza.toppings)
-//     //         {
-//     //             var topping = pizza.toppings[index];
-//     //             output += '<li>' + topping.name + '</li>';
-//     //         }
-//     //         $('#pizza').html(output); //update the DOM with the new output
-//     //     }
-//     //     updateDomPizza(pizza); //invoke function definition
-
-
-
-
-
-
-
-
-
-//     // $('#addTopping').on('click', function(){
-
-//     //     var toppingName = $('#topping').val();
-
-//     //     console.log('top from input', toppingName);
-//     //     pizza.addTopping(toppingName);
-//     //     updateDomPizza(pizza); //invoke function definition
-//     //     // $('#topping').val('');
-//     // });
-
-
-// });
